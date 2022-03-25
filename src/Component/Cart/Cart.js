e4174634818b231c6cc84019e90e9867adf7d5ff
ahmed-nasir <nasirahmed595@gmail.com>
2022-03-25 09:41:51 +0600

    correction calculation

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -4,16 +4,17 @@ import './Cart.css'
 const Cart = ({ cart }) => {
     console.log(cart);
     let total = 0;
-    let shipping=0;
-    let tax=0;
-    let grandTotal=0;
+    let shipping = 0;
+    let grandTotal = 0;
     for (const product of cart) {
         console.log(product.price)
-        total = total+product.price;
-        shipping =shipping+product.shipping;
-        tax=total*0.1;
-        grandTotal=total+shipping+tax;
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
     }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    grandTotal = total + shipping + tax;
+
     return (
         <div className='cart'>
             <h1>
